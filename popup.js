6d72b9757edacf75c28b44ffdeb364ee12cb4c35
Ridhi  Gupta <ridhigupta1603@gmail.com>
2024-05-22 23:10:58 +0530

    Removin hello world extension and adding basic htmln

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("This is a popup!")
+// adding an eventListener on button
+document.getElementById('summarizeButton')
+.addEventListener('click', async() => 
+// executiing this following code as sooon as the 
+// button is clicked
+{ const inputText=document.getElementById('inputText').value;
+
+//console.log(inputText+"hi i am inputtext")
+
+// summaarixiing the inputTxt
+const summary="summary"
+
+// assigning the summarized value to outputTextArea
+document.getElementById('outputText').value=summary
+
+})
