cf2f328d0470faa0f81addff1fd3046414dd0d65
Ridhi  Gupta <ridhigupta1603@gmail.com>
2024-05-24 23:53:53 +0530

    Removin hello world extension and adding basic htmln

--- popup.js
+++ popup.js
@@ -6,11 +6,52 @@ document.getElementById('summarizeButton')
 { const inputText=document.getElementById('inputText').value;
 
 //console.log(inputText+"hi i am inputtext")
+if(inputText){
+    console.log('Input Text is' , inputText);
 
-// summaarixiing the inputTxt
-const summary="summary"
+// summaarixiing the inputTxt using HUGGING FACE
+const summary=await summarizeText(inputText);
 
 // assigning the summarized value to outputTextArea
-document.getElementById('outputText').value=summary
+document.getElementById('outputText').value=summary;
+}
 
-})
+// if no inputText is passed
+else{
+    alert('please paste the text here');
+}
+});
+
+
+
+async function summarizeText(text) {
+
+    const apiUrl='https://api-inference.huggingface.co/models/facebook/bart-large-cnn'; // end point of api
+    const apiKey='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'; // got this from huggingface
+
+    const response=await fetch(apiUrl, {
+   
+        method: 'POST' ,
+        headers:{
+         'Authorization': `Bearer ${apiKey}`,
+         'Content-Type':'application/json'
+        } , 
+        body: JSON.stringify({
+            inputs: text
+          })
+    });
+
+
+    if(!response.ok){
+      throw new Error('Error summarizing Text');
+
+    }
+
+ console.log('Logging response',  response);
+
+ const data=await response.json();
+
+ console.log('Logging data', data);
+ return data[0].summary_text;
+}
+
